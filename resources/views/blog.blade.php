291dca642fbc4afc906af4a23335bb849d4a3f3b
pabloacisera <pabloacisera2021@gmail.com>
2025-01-21 21:42:05 -0300

    21012025_

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,1 +1,15 @@
-<div>Blogs</div>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+    @vite('resources/css/app.css')
+</head>
+
+<body class="bg-gray-100 text-gray-800 font-sans">
+    <h1 class="text-4xl font-bold text-center text-blue-600 my-8">Into the Way</h1>
+    @include('partials.navigation')
+    <div class="container mx-auto px-4">
+        <h2 class="text-2xl font-bold text-blue-600 my-4">Blog</h2>
+    </div>
+</body>
